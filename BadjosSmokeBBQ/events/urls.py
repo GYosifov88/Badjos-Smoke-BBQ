5d97d9a2f4735dd8bf4760c7649038dfe0732a85
GYosifov88 <gyosifov88@gmail.com>
2023-06-22 10:54:59 +0300

    implement events page

--- BadjosSmokeBBQ/events/urls.py
+++ BadjosSmokeBBQ/events/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = ()
+from django.urls import path
+
+from BadjosSmokeBBQ.events.views import EventsListView
+
+urlpatterns = (
+    path('', EventsListView.as_view(), name='events'),
+)
--- BadjosSmokeBBQ/events/views.py
+++ BadjosSmokeBBQ/events/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from . import models
+from .models import Event
+from ..articles.models import Article
 
-# Create your views here.
+
+class EventsListView(ListView):
+    context_object_name = 'events'
+    model = models.Event
+    template_name = 'events/events.html'
+
+    def get_context_data(self, *args, **kwargs, ):
+        context = super().get_context_data(*args, **kwargs)
+        context['events'] = Event.objects.all()
+        context['articles'] = Article.objects.all()
+        return context
